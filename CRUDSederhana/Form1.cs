dfc1aecdb413f2a734b6e604ef4c65f7c5fc6ae7
Kevin Adrian <kevin19305.ib@gmail.com>
2025-03-20 15:39:44 +0700

    buat void button tambah

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -59,4 +59,10 @@ namespace CRUDSederhana
                 }
             }
         }
-    
+        private void BtnTambah(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+  
