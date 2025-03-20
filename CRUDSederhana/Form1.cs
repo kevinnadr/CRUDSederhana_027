da23d6a8b1f74a66f37d672af38f3e89305b714d
Kevin Adrian <kevin19305.ib@gmail.com>
2025-03-20 15:35:26 +0700

    buat bagian private void form1

--- CRUDSederhana/Form1.cs
+++ CRUDSederhana/Form1.cs
@@ -12,3 +12,17 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace CRUDSederhana
 {
+    public partial class Form1 : Form
+    {
+        private string ConnectionString = "Data Source = MSI\\KEVINNADR;" + "Initial Catalog =OrganisasiMahasiswa;" + "Integrated Security = true";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+ 
